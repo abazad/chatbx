1212fce68a76f9ae5735e088e9ca8e33561ac0ad
jhayrdmz <dmz.gfx@gmail.com>
2013-10-06 20:20:20 +0800

    Delete autoloader.php

--- lib/autoloader.php
+++
@@ -1,7 +0,0 @@
-<?php
-function __autoload($class_name) {
-    $path = DIR_ROOT . '/lib/classes/' . strtolower($class_name) . '.class.php';
-    if(file_exists($path)) {
-        include($path);
-    }
-}
